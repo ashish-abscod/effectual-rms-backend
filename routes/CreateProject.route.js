34b4ec2f4b9c253c3c04db01a8dbb4f8384f2783
ashish sharma <ashish.sharma@abscod.com>
2022-09-12 14:34:23 -0700

    Deleted files

--- routes/CreateProject.route.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { CreateProject } = require('../controllers/CreateProject.controller');
-
-router.post('/createProject', CreateProject )
-
-module.exports = router;
