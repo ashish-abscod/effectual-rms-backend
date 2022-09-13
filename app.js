98f8bf56e2822514ea30367573c1868488517fc5
tamanna223 <tamannab931@gmail.com>
2022-09-13 18:09:17 +0530

    make apis for feedback,project,user

--- app.js
+++ app.js
@@ -1,44 +1,48 @@
-const express = require('express');   //imported express
-const app = express();    //created an express app
-const mongoose = require('mongoose');  //mongoose for connection
-require('dotenv/config');   //config file
-const bodyParser = require('body-parser');  //used to parse the requested data body
-app.use(bodyParser.json());   //this will parse the body recieved from req object in json format.
-const projects = require('./routes/projects.route');
-const users = require('./routes/users.route');
-const cors = require('cors');
+const express = require("express"); //imported express
+const app = express(); //created an express app
+const mongoose = require("mongoose"); //mongoose for connection
+require("dotenv/config"); //config file
+const bodyParser = require("body-parser"); //used to parse the requested data body
+app.use(bodyParser.json()); //this will parse the body recieved from req object in json format.
+const projects = require("./routes/projects.route");
+const users = require("./routes/users.route");
+const feedback = require("./routes/feedback.route");
+const cors = require("cors");
 
 //---------------Mongodb Connection -----------------
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DB_Connection, {
-    useNewUrlParser: true
-}, (err) => {
+mongoose.connect(
+  process.env.DB_Connection,
+  {
+    useNewUrlParser: true,
+  },
+  (err) => {
     if (!err) {
-        console.log('MongoDB Connection Succeeded.')
+      console.log("MongoDB Connection Succeeded.");
     } else {
-        console.log('Error in DB connection: ' + err)
+      console.log("Error in DB connection: " + err);
     }
+  }
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello Express.js");
 });
 
-
-
-app.get('/', (req, res) => {
-    res.send("Hello Express.js");
-})
-
 // ----------------------middleware cors---------------
-app.use(cors(
-    {
-        origin: 'http://localhost:3000',
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        optionsSuccessStatus: 204
-    }
-));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    optionsSuccessStatus: 204,
+  })
+);
 
-app.use('/projects', projects);
-app.use('/users', users);
+app.use("/projects", projects);
+app.use("/users", users);
+app.use("/feedback", feedback);
 
 const Port = process.env.port;
 app.listen(Port, () => {
-    console.log(`Server is listening on port ${Port}`)
-})
+  console.log(`Server is listening on port ${Port}`);
+});
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,20 +1,37 @@
-const bcrypt = require('bcryptjs');
-const UsersModel = require("../models/users.model")
+const bcrypt = require("bcryptjs");
+const UsersModel = require("../models/users.model");
 
 exports.createUser = async (req, res) => {
-    const data = UsersModel({
-        username : req.body.username,
-        password : bcrypt.hashSync(req.body.password, 10),
-        name : req.body.name,
-        email : req.body.email,
-        role : req.body.role,
-        status : req.body.status
-    })
+  const data = UsersModel({
+    username: req.body.username,
+    password: bcrypt.hashSync(req.body.password, 10),
+    name: req.body.name,
+    email: req.body.email,
+    role: req.body.role,
+    status: req.body.status,
+  });
+  try {
+    const savedUser = await data.save();
+    res.json({ message: "User was registered successfully!" });
+    res.send(data);
+  } catch (error) {}
+};
 
-    try {
-        const savedUser = await data.save();
-        res.json({ message: "User was registered successfully!" });
-    } catch (error) {
-        
-    }
-}
+exports.getUser = async (req, res) => {
+  let item = await UsersModel.find();
+  if (item.length > 0) {
+    res.send(item);
+  } else {
+    res.send({ result: "data not found" });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  const result = await UsersModel.updateOne(
+    { _id: req.params.id },
+
+    { $set: { status: "Inactive" } }
+  );
+  res.send(result);
+  console.log(req.params.id);
+};
