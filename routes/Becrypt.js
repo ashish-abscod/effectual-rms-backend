7826af4e7a07a28547cf2483b00847775250df61
ashish sharma <ashish.sharma@abscod.com>
2022-09-13 18:04:58 -0700

    commented becrypt file

--- routes/Becrypt.js
+++ routes/Becrypt.js
@@ -1,3 +1,34 @@
-// const route = require('express').Router();
+// const usersModel = require('../models/users.model');
+// const bcrypt = require('bcryptjs');
 
-// route.()
+// const router = require('express').Router();
+
+// router.get('/', async (req, res)=> {
+//     try {
+//         const data = await usersModel.find({},{password : 1,_id:1});
+
+//         try{
+//         data.map(async (item)=> {
+//             let newPassword = await bcrypt.hashSync(item.password, 10);
+//             return await usersModel.findByIdAndUpdate(item._id, {password : newPassword},function (err,docs) {
+//                 if (err){
+//                     console.log(err)
+//                 }
+//                 else{
+//                     console.log("Updated Password for  : ", docs);
+//                 }} )
+//             console.log("Updated Password for id - " + item._id);
+//         })
+//         res.json("Successfully updated passwords.");
+//         return;
+//     }catch(err){
+//         res.json(err);
+//     }
+
+
+//     } catch (error) {
+//         console.log({message : error})
+//     }
+// })
+
+// module.exports = router;
