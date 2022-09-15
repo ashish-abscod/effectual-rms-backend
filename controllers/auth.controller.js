f6c401bdbf36ff1d4389db9ca1528c6172719cde
ashish sharma <ashish.sharma@abscod.com>
2022-09-14 17:55:17 -0700

    Created sigIn functionality

---
+++ controllers/auth.controller.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const usersModel = require("../models/users.model");
+require("dotenv").config({ path: "../.env" });
+
+
+exports.signIn = async (req, res) => {
+    try {
+        const user = await usersModel.findOne({ email: req.body.email });
+        if (!user)
+            return res
+                .status(400)
+                .json({ error: "Invalid user", data: null, code: 404 });
+
+        if (user.password !== req.body.password)
+            return res
+                .status(400)
+                .json({ error: "Invalid credential", data: null, code: 404 });
+
+        const token = await jwt.sign(
+            { _id: user._id.toString() }, //payload
+            process.env.SUPER_SECRET_KEY
+            );
+
+        user.password = undefined;
+        res.json({ data: { user, token }, err: null, code: 200 });
+    } catch (error) {
+        console.log("Error:", error.message);
+        res.json({ error: "Something went wrong", data: null, code: 500 });
+    }
+}
