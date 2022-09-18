d4dbaed531556724ad1b32ab27ea9fd824d69e97
aeshtech <dev.aeshtech@gmail.com>
2022-09-18 16:37:15 +0530

    Created Replies Schema.

---
+++ models/Replies.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");   //Erase if already required
+
+const commentSchema = new mongoose.Schema({
+
+commentId : {
+    type : String
+},
+replie : {
+    type : String
+},
+time : {
+    type : String
+},
+userName : {
+    type : String
+},
+userRole : {
+    type : String
+}
+
+})
+
+//pass third argument as schema name if exactly what you want
+module.exports = mongoose.model("Comments", commentSchema, "Comments");
