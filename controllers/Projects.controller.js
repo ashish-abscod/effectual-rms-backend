04ed71884e056437b2f1e427789d3a8e35bb2bef
ashish-abscod <ashish.sharma@abscod.com>
2022-09-20 14:53:36 +0530

    removed console for body

--- controllers/Projects.controller.js
+++ controllers/Projects.controller.js
@@ -12,7 +12,7 @@ exports.getProjects = async (req, res) => {
 
 exports.getOneProject = async (req, res) => {
   try {
-    const data = await projectModel.findOne({projectId : req.params.id});
+    const data = await projectModel.findOne({ projectId: req.params.id });
     res.json(data);
   } catch (error) {
     res.send(error);
@@ -63,38 +63,37 @@ exports.createProject = async (req, res) => {
 };
 
 exports.updateProject = async (req, res) => {
-  try{
-  console.log(req.body)
-  const result = await projectModel.findOneAndUpdate({projectId : req.params.id},
-    {
-      searchObject: req.body.SearchObject,
-      claims: req.body.ClaimsToBeSearched,
-      reqDelivery: req.body.RequirementForDelivery,
-      projectName: req.body.ProjectName,
-      requesterName: req.body.requesterName,
-      deliveryDate: req.body.RequirementDeliveryDate,
-      priorArtDate: req.body.PriorArtCuttOffDate,
-      emailContent: req.body.EmailContent,
-      info: req.body.UsefulInformationForSearch,
-      status: req.body.Status,
-      projectManager: req.body.ProjectManager,
-      requestedDate: req.body.RequestedDate,
-      patentNumber: req.body.PatentNumber,
-      createdById: req.body.CreatedById,
-      completedDate: req.body.CompletedDate,
-      jurisdiction: req.body.Jurisdiction,
-      include: req.body.Include,
-      technicalField: req.body.TechnicalField,
-      standard: req.body.StandardRelated,
-      sso: req.body.SSONeeded,
-      usipr: req.body.USIPRSpecial,
-      impClaim: req.body.ImportantClaims,
-      nonImpClaim: req.body.UnimportantClaims
-    }
-  );
-  res.send({Message:"Successfully updated project!", code : 200});
+  try {
+    const result = await projectModel.findOneAndUpdate({ projectId: req.params.id },
+      {
+        searchObject: req.body.SearchObject,
+        claims: req.body.ClaimsToBeSearched,
+        reqDelivery: req.body.RequirementForDelivery,
+        projectName: req.body.ProjectName,
+        requesterName: req.body.requesterName,
+        deliveryDate: req.body.RequirementDeliveryDate,
+        priorArtDate: req.body.PriorArtCuttOffDate,
+        emailContent: req.body.EmailContent,
+        info: req.body.UsefulInformationForSearch,
+        status: req.body.Status,
+        projectManager: req.body.ProjectManager,
+        requestedDate: req.body.RequestedDate,
+        patentNumber: req.body.PatentNumber,
+        createdById: req.body.CreatedById,
+        completedDate: req.body.CompletedDate,
+        jurisdiction: req.body.Jurisdiction,
+        include: req.body.Include,
+        technicalField: req.body.TechnicalField,
+        standard: req.body.StandardRelated,
+        sso: req.body.SSONeeded,
+        usipr: req.body.USIPRSpecial,
+        impClaim: req.body.ImportantClaims,
+        nonImpClaim: req.body.UnimportantClaims
+      }
+    );
+    res.send({ Message: "Successfully updated project!", code: 200 });
   }
-  catch(err){
+  catch (err) {
     res.send(err)
   }
 };
