206050c6c7cbcbde58be70724d4e023f85bf58e4
tamanna223 <tamannab931@gmail.com>
2022-09-20 15:41:26 +0530

    make assigneduser schema and api for that

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,37 +1,34 @@
 const bcrypt = require("bcryptjs");
 const usersModel = require("../models/Users.model");
-const {cloudinary} = require("./Files.controller");
+const { cloudinary } = require("./Files.controller");
 const jwt = require("jsonwebtoken");
 
 exports.createUser = async (req, res) => {
-try{
-    const uploadResponse = await cloudinary.uploader.upload(req.body.picture,{
+  try {
+    const uploadResponse = await cloudinary.uploader.upload(req.body.picture, {
       upload_preset: "attachments",
-     });
- 
-     const data = usersModel({
-         password : req.body.password,
-         name : req.body.name,
-         email : req.body.email,
-         role : req.body.role,
-         status : req.body.status,
-         picture:uploadResponse.secure_url
-     })
-     try{
-     await data.save()
-     res.json({ data: data, err: null, code: 200 });
-   }
-   
-   catch(error){
-    console.log("error: ", error);
-    res.json({ error: error, data: null, code: 403 });
-   }
- 
-  }catch(error){
+    });
+
+    const data = usersModel({
+      password: req.body.password,
+      name: req.body.name,
+      email: req.body.email,
+      role: req.body.role,
+      status: req.body.status,
+      picture: uploadResponse.secure_url,
+    });
+    try {
+      await data.save();
+      res.json({ data: data, err: null, code: 200 });
+    } catch (error) {
+      console.log("error: ", error);
+      res.json({ error: error, data: null, code: 403 });
+    }
+  } catch (error) {
     console.log("error: ", error);
     res.json({ error: error, data: null, code: 500 });
-   }
-}
+  }
+};
 
 exports.getUsers = async (req, res) => {
   let item = await usersModel.find();
@@ -50,7 +47,7 @@ exports.deleteUser = async (req, res) => {
   );
   res.send(result);
   console.log(req.params.id);
-}
+};
 
 exports.SearchUser = async (req, res) => {
   let result = await usersModel.find({
@@ -68,28 +65,23 @@ exports.getUserName = async (req, res) => {
   }
 };
 
+exports.updateUser = async (req, res) => {
+  let result = await usersModel.updateOne(
+    { _id: req.params.id },
+    { $set: req.body }
+  );
+  res.send(result);
+};
 
-exports.getForgotPassWord = async (req,res) => {
+exports.getForgotPassWord = async (req, res) => {};
 
-}
+exports.postCreatePassWord = async (req, res) => {
+  const { email } = req.body;
 
-exports.postCreatePassWord = async (req,res) => {
+  if (email !== usersModel.email) {
+  }
+};
 
-const {email} = req.body;
+exports.getCreatedPassword = async (req, res) => {};
 
-if(email !== usersModel.email){
-  
-}
-
-
-}
-
-
-exports.getCreatedPassword = async(req,res) => {
-
-}
-
-exports.postResetPassWord = async (req,res) => {
-
-}
-
+exports.postResetPassWord = async (req, res) => {};
