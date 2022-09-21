ece9da612ca8b2df4d2482eb177e7b8c94a255b4
ashish-abscod <ashish.sharma@abscod.com>
2022-09-21 14:58:25 +0530

    created discussion controller

--- controllers/Discussion.js
+++ controllers/Discussion.js
@@ -2,7 +2,9 @@ const commentModel = require('../models/Comments.model');
 
 exports.getDiscussion = async (req, res) => {
     try {
-        const data = await commentModel.find({projectId : req.params.projectId});
+        const data = await commentModel.find({projectId : req.params.projectId}).populate({
+          path: "myReplies",
+        });;
         res.json(data);
       } catch (e) {
         res.send("Error - " + e);
