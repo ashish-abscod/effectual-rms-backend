64928933ea029756f6bcac986e366c08e23b0cab
ashish-abscod <ashish.sharma@abscod.com>
2022-09-21 15:17:09 +0530

    removed ;

--- controllers/Discussion.js
+++ controllers/Discussion.js
@@ -4,7 +4,7 @@ exports.getDiscussion = async (req, res) => {
     try {
         const data = await commentModel.find({projectId : req.params.projectId}).populate({
           path: "myReplies",
-        });;
+        });
         res.json(data);
       } catch (e) {
         res.send("Error - " + e);
