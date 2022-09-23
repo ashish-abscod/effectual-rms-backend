e45a95483e53a86ee76108463181f15c8c13a009
ashish-abscod <ashish.sharma@abscod.com>
2022-09-23 19:11:58 +0530

    fixed comment controller

--- controllers/Comments.controller.js
+++ controllers/Comments.controller.js
@@ -2,14 +2,10 @@ const CommentsModel = require("../models/Comments.model");
 
 
 exports.createComment = async(req,res) => {
-    const data = new CommentsModel({
-        comment: req.body.comment,
-        
-    })
-
     try {
-        const dataToSave = await data.save();
-        res.status(200).json(dataToSave)
+        const data = new CommentsModel(req.body)
+        const result = await data.save();
+        res.status(200).json(result)
     }
     catch (error) {
         res.status(400).json({message: error.message})
