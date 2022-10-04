623baec860808a65ad78ab9c39b25d4ff0ed7eda
ashish sharma <ashish.sharma@abscod.com>
2022-10-04 15:55:37 +0530

    created a token model with expiration time

--- models/Token.model.js
+++ models/Token.model.js
@@ -1,24 +1,21 @@
 const mongoose = require("mongoose"); 
 const { Schema, model } = mongoose;
-// Erase if already required
 
 // Declare the Schema of the Mongo model
 const tokenSchema = new Schema({
     userId: {
         type:Schema.Types.ObjectId,
         required: true,
-        ref: "Users",
-    },
-    token: {
-        type: String,
-        required: true,
     },
     createdAt: {
         type: Date,
-        default: Date.now,
-        expires: 3600,
+        default : Date.now,
+        index : {expires : 3600}
+    },
+    token:{
+        type : String ,
+        required: true
     },
 });
 
-const Token = mongoose.model("token", tokenSchema,"token");
-module.exports = Token
+module.exports= mongoose.model("Tokens", tokenSchema,"Tokens");
