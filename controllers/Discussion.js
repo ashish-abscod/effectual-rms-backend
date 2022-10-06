4e2de0cb369949adc8936db5b8f9562930e33251
ashish sharma <ashish.sharma@abscod.com>
2022-10-06 15:33:29 +0530

    deep population for replieAttachement  in getDiscussion

--- controllers/Discussion.js
+++ controllers/Discussion.js
@@ -2,14 +2,23 @@ const commentModel = require('../models/Comments.model');
 require('../models/Replies.model');
 
 exports.getDiscussion = async (req, res) => {
-    try {
-        const data = await commentModel.find({projectId : req.params.projectId}).populate({
-          path: "myReplies",
-        });
-        res.json(data);
-      } catch (e) {
-        res.send("Error - " + e);
+  try {
+    const data = await commentModel.find({ projectId: req.params.projectId }).populate({
+      path: "replies",
+      //deep population for replie attachments
+      populate: {
+        path: "attachments",
+      },
+    }).populate(
+      {
+        //populating comment attachments
+        path: "attachments"
       }
+    );
+    res.json(data);
+  } catch (e) {
+    res.send("Error - " + e);
+  }
 }
 // exports.getDiscussion = async (req, res) => {
 //     try {
