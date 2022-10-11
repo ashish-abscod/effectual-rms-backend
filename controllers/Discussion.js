58bd349f5e99afa6c4896127e96c3d6d659db1af
Bandana <bandanasatpathy435@gmail.com>
2022-10-11 17:49:11 +0530

    fixed comment based on time

--- controllers/Discussion.js
+++ controllers/Discussion.js
@@ -3,16 +3,14 @@ require("../models/Replies.model");
 
 exports.getDiscussion = async (req, res) => {
   try {
-    const data = await commentModel
-      .find()
-      .sort("time")
-      .find({ projectId: req.params.projectId })
+    const data = await commentModel.find({ projectId: req.params.projectId })
+      .sort({"time":"desc"})
       .populate({
         path: "replies",
         //deep population for replie attachments
         populate: {
           path: "attachments",
-        },
+        }
       })
       .populate({
         //populating comment attachments
@@ -23,6 +21,18 @@ exports.getDiscussion = async (req, res) => {
     res.send("Error - " + e);
   }
 };
+
+exports.getFilesByRole = async (req, res) => {
+ 
+  try {
+      const effectualAdmin = await commentAttachmentModel.find({projectId: req.params.id})
+      res.json(effectualAdmin);
+      res.json({error})
+    } catch (error) {
+    res.send(error);
+  }
+};
+
 // exports.getDiscussion = async (req, res) => {
 //     try {
 //         const data = await RepliesModel.find({projectId : req.params.projectId})
