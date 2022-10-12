2fd9bfbb4d547f17e8f395d9adcd4642ebd554bc
ashish sharma <ashish.sharma@abscod.com>
2022-10-12 11:34:05 +0530

    removed route for create evaluation

--- routes/Evaluation.js
+++ routes/Evaluation.js
@@ -1,11 +1,9 @@
 const router = require("express").Router();
 const {
-  createEvaluation,
   getEvaluation,
   evaluationUpdate,
 } = require("../controllers/Evaluation");
 
-router.post("/", createEvaluation);
 router.get("/getById/:id", getEvaluation);
 router.post("/:projectId", evaluationUpdate);
 module.exports = router;
