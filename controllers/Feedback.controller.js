e7dc6de2c3d2ff6599fa975b4c54c2ae9cbfa527
ashish sharma <ashish.sharma@abscod.com>
2022-10-12 11:33:30 +0530

    fixed feedback and created response msg

--- controllers/Feedback.controller.js
+++ controllers/Feedback.controller.js
@@ -1,7 +1,12 @@
 const feedbackModel = require("../models/Feedbacks.model");
 
 exports.createFeedback = async (req, res) => {
+  try{
   let add = new feedbackModel(req.body);
   let result = await add.save();
-  res.send(result);
+  if(result) return res.json({msg:"Successfully sent feedback.", status:"success"});
+  else return res.json({msg:"Sory, feedback not sent.", status:"failed"});
+  }catch{
+    return res.json({msg:"Something went Wrong", status:"failed"});
+  }
 };
