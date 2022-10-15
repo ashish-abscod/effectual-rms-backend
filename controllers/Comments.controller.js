ba3cd3a7a65dbc7e2408a0729596ed016ae3b795
ashish sharma <ashish.sharma@abscod.com>
2022-10-15 16:01:54 +0530

    formated date using external package

--- controllers/Comments.controller.js
+++ controllers/Comments.controller.js
@@ -1,19 +1,21 @@
 const CommentsModel = require("../models/Comments.model");
 const ObjectID = require("bson-objectid");
+const date = require('date-and-time');
 
 exports.createComment = async(req,res) => {
+    const now = new Date();
     const data = new CommentsModel({
         commentId : ObjectID(),
         projectId : req.body.projectId,
         comment: req.body.content,
-        time : req.body.time,
+        time : date.format(now, "YYYY-MM-DD HH:mm:ss"),
         userName : req.body.userName,
         userRole : req.body.userRole
     })
     
     try {
         const result = await data.save();
-        res.json({result, msg: "Successfully posted comment.", status: "success"})
+        res.json({...result._doc, msg: "Successfully posted comment.", status: "success"})
     }
     catch (error) {
         res.status(400).json({msg : "Sorry, something went wrong. Comment posting failed!" , status: "failed" })
